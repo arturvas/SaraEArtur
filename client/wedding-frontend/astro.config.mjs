1ce4a1d9affedf0a28b99fa0651ca971757c2ae4
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2025-07-10 20:31:09 -0400

    chore: integrate astro-icon in astro.config.mjs

--- client/wedding-frontend/astro.config.mjs
+++ client/wedding-frontend/astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [icon()],
 });
