1bf03515b9618c72531bf5f0ace45e213582f46f
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2025-07-20 13:30:23 -0400

    feat: expand `gifts-Data` with new categories, items, and change button name on `GiftsSection`

--- client/wedding-frontend/src/data/gifts-data.js
+++ client/wedding-frontend/src/data/gifts-data.js
@@ -1,22 +1,47 @@
 export const giftsData = {
     categories: [
         {
-            id: 'cozinha',
-            title: 'Cozinha',
-            description: 'Ajude a montar os itens básicos da nossa cozinha!',
+            id: 'utensilios-cozinha',
+            title: 'Utensílios da Cozinha',
+            description: 'Esses são os detalhes que vão dar vida às nossas refeições e deixar a cozinha pronta para receber com carinho.',
             items: [
-                { id: 'panela', title: 'Jogo de Panelas', price: 500 },
-                { id: 'liquidificador', title: 'Liquidificador', price: 300 },
-                { id: 'copos', title: 'Conjunto de Copos', price: 150 },
+                { id: 'kit-copo-jarra', title: 'Kit copo e jarra', price: 120 },
+                { id: 'travessa', title: 'Travessa de vidro', price: 80 },
+                { id: 'jg-xicaras', title: 'Jogo de xícaras', price: 142 },
+                { id: 'escorredor', title: 'Escorredor de louça', price: 150 },
+                { id: 'panela-pressao', title: 'Panela de pressão', price: 130 },
+                { id: 'jg-panelas', title: 'Jogo de panelas', price: 400 },
+            ],
+        },
+      {
+            id: 'eletroportateis',
+            title: 'Eletroportáteis',
+            description: 'Com esses aliados, nosso dia a dia ficará mais prático e saboroso.',
+            items: [
+                { id: 'liquidificador', title: 'Liquidificador', price: 170 },
+                { id: 'ferro-passar', title: 'Ferro de passar', price: 200 },
+                { id: 'air-fryer', title: 'Air Fryer', price: 542 },
+                { id: 'microondas', title: 'Microondas', price: 570 },
+                { id: 'filtro-agua', title: 'Filtro de água', price: 650 },
             ],
         },
         {
             id: 'cama-banho',
             title: 'Cama & Banho',
-            description: 'Conforto para o nosso dia a dia!',
+            description: 'Itens que vão tornar nosso descanso ainda mais aconchegante e nosso lar mais acolhedor desde o primeiro dia.',
             items: [
-                { id: 'lencois', title: 'Lençóis 400 fios', price: 600 },
-                { id: 'toalhas', title: 'Toalhas de Banho', price: 200 },
+                { id: 'jg-cama', title: 'Jogo de cama', price: 350 },
+                { id: 'toalhas', title: 'Toalhas de Banho', price: 90 },
+            ],
+        },
+        {
+            id: 'casa-utilidades',
+            title: 'Casa & Utilidades',
+            description: 'Itens essenciais para montar e manter um lar completo, funcional e cheio de cuidado.',
+            items: [
+                { id: 'jg-tapete', title: 'Jogo de tapetes', price: 99 },
+                { id: 'fogao', title: 'Fogão', price: 900 },
+                { id: 'geladeira', title: 'Geladeira', price: 2000 },
             ],
         },
         {
