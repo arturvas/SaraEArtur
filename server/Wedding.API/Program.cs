b3318074826ed7054deae45e5d1a045d504fd25a
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2025-07-26 19:52:50 -0400

    chore: comment out automatic migrations in Program.cs and seed gifts

--- server/Wedding.API/Program.cs
+++ server/Wedding.API/Program.cs
@@ -193,7 +193,7 @@ using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     
-    db.Database.Migrate();
+    // db.Database.Migrate();
     GiftSeeder.Seed(db);
 }
 
