d86b6449b8195d26b3220b2a02dfacc0503924ff
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2026-03-24 19:38:26 -0300

    chore: remove server-side Wedding.API-related files and dependencies

--- server/Wedding.API/Core/Models/Gift.cs
+++
@@ -1,9 +0,0 @@
-namespace Wedding.API.Core.Models;
-
-public class Gift
-{
-    public int Id { get; set; }
-    public string Category { get; set; } = string.Empty;
-    public string Title { get; set; } = string.Empty;
-    public decimal Price { get; set; }
-}
